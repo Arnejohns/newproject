845dbc99e45340eade505a3ae4d33717ca5aab96
charitha <rcharitha31@gmail.com>
2023-12-15 12:39:31 +0530

    Demo for Team

---
+++ src/main/java/com/app/user/newproject/App.java
@@ -0,0 +1,13 @@
+package com.app.user.newproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
